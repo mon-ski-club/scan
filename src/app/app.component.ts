7311a9aced157f20a9c522b516659639bd7b094f
Jlnbnt <benat.julien@gmail.com>
2023-11-13 13:53:34 +0100

    Workplace setup : config && linters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'scan';
+  title = 'SCAN'
 }
