8c3c6de8967f86d22f3197428f2d92303933ad41
Jérôme Pochat <jeromepochat@gmail.com>
2023-11-14 23:58:16 +0100

    Introduce person entity

---
+++ src/app/person/person.ts
@@ -0,0 +1,14 @@
+export interface Person {
+  id: string
+  firstName: string
+  lastName: string
+  gender: string
+  birthDate: Date
+  address: string
+  zip: string
+  city: string
+  email: string
+  phone1: string
+  phone2: string
+  archived: boolean
+}
