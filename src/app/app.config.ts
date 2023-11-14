a998452bfc5f7eec240b1ca367342f7c808c5b9e
Jérôme Pochat <jeromepochat@gmail.com>
2023-11-14 22:13:16 +0100

    Fix application providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,21 +8,10 @@ import { SettingsService } from './settings/settings.service'
 import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideDatabase()],
-}
-
-export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
-    {
-      provide: APP_INITIALIZER,
-      useFactory:
-        (settingsService: SettingsService, httpClient: HttpClient) => () =>
-          createDatabase(settingsService, httpClient),
-      multi: true,
-      deps: [SettingsService, HttpClient],
-    },
+    provideDatabase(),
   ],
 }
