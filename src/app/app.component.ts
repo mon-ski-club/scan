c3609745dffc6628521453ca50ff5a20e3547931
Jlnbnt <benat.julien@gmail.com>
2023-11-17 14:03:36 +0100

    move svg logic from app to layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
-import { HttpClientModule } from '@angular/common/http'
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon'
-import { DomSanitizer } from '@angular/platform-browser'
 import { DatabaseService } from './core/database/database.service'
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, MatIconModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-    private databaseService: DatabaseService,
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'scan',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        '../assets/logos/iso-scan.svg',
-      ),
-    )
-
+  constructor(private databaseService: DatabaseService) {
     /* Initializing Database */
     this.databaseService.initializeDatabase()
   }
